0bb1af7e63098f1cb7f17b7666c9fe3188ef7135
Willian Werlang <willianwerlang@hotmail.com>
2022-08-19 21:28:37 -0300

    Upload project from Spring Initializr

---
+++ src/main/java/com/wwerlang/learnspringhibernate/LearnSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.wwerlang.learnspringhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wwerlang/learnspringhibernate/LearnSpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwerlang.learnspringhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
