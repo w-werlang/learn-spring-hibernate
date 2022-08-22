8f897dd78fda42a4776a54898a382ae17ccd97a7
Willian Werlang <willianwerlang@hotmail.com>
2022-08-22 00:45:08 -0300

    Rename Application class

--- src/main/java/com/wwerlang/learnspringhibernate/Application.java
+++ src/main/java/com/wwerlang/learnspringhibernate/Application.java
@@ -2,12 +2,14 @@ package com.wwerlang.learnspringhibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class LearnSpringHibernateApplication {
+@ComponentScan
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringHibernateApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
